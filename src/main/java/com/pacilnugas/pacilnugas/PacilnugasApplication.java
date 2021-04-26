95f4ea2fdc443cb9bbe4568a89cf5a918752e53b
cornelita_ <lugitasantoso127@gmail.com>
2021-04-26 21:55:09 +0700

    Start Project Pacil Nugas!

---
+++ src/main/java/com/pacilnugas/pacilnugas/PacilnugasApplication.java
@@ -0,0 +1,13 @@
+package com.pacilnugas.pacilnugas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacilnugasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PacilnugasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pacilnugas/pacilnugas/PacilnugasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacilnugas.pacilnugas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacilnugasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
