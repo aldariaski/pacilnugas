460114456d7bff89158dd68d61b86916dcfdf35b
Yusuf Fakhri Aldrian <aldrianyf@gmail.com>
2021-04-30 23:39:24 +0700

    Added from lita's, to add repo and inputting stuff

--- src/main/java/com/pacilnugas/landingpage/repository/AssignmentRepository.java
+++ src/main/java/com/pacilnugas/landingpage/repository/AssignmentRepository.java
@@ -3,5 +3,6 @@ package com.pacilnugas.landingpage.repository;
 import com.pacilnugas.landingpage.model.AssignmentFake;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//TODO: Add content repo
 public interface AssignmentRepository extends JpaRepository<AssignmentFake, Integer> {
 }
