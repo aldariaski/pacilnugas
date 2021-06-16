5425dc3a719555d81b32504213b3b868745d897b
Syabib Ash-Shiddiqi <sasfort14@gmail.com>
2021-06-16 13:44:11 +0700

    fix for final

--- src/main/java/com/pacilnugas/PacilnugasApplication.java
+++ src/main/java/com/pacilnugas/PacilnugasApplication.java
@@ -1,4 +1,4 @@
-package com.pacilnugas.pacilnugas;
+package com.pacilnugas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
